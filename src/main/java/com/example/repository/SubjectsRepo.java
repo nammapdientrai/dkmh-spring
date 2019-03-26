523dbc1169ac5721f895cf6976b5b4e90c70a11e
nammapdientrai@gmail.com <nammapdientrai@gmail.com>
2019-03-26 14:25:45 +0700

    26-03-2019

--- src/main/java/com/example/repository/SubjectsRepo.java
+++ src/main/java/com/example/repository/SubjectsRepo.java
@@ -1,9 +1,12 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.entity.Subjects;
 
 public interface SubjectsRepo extends JpaRepository<Subjects, String> {
 
+	@Query()
+	public void get();
 }
